d2b94f1dc476940d2a6b1efae9747e2c4cd6cc3a
Luis De Anda <107770749+LuisDA39@users.noreply.github.com>
2024-06-19 13:11:14 -0600

    Create Main.java

---
+++ age_calculator/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Age age1 = new Age(23.85);
+        Age age2 = new Age(2002, 9, 1);
+        System.out.println(age1.print());
+        System.out.println(age2.print());
+    }
+}
